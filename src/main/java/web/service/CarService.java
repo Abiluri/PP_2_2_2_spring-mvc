4f943678a5c91fe3f4570b14fe32907be0784f7a
Abiluri <Wilbury2@gmail.com>
2024-12-06 00:36:25 +0800

    .

--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,28 +1,11 @@
 package web.service;
 
-import org.springframework.stereotype.Service;
 import web.model.Car;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Service
-public class CarService {
-    private List<Car> cars = new ArrayList<>();
 
-    public CarService() {
-        cars.add(new Car("Toyota Camry", 2020, "Silver"));
-        cars.add(new Car("Honda Accord", 2019, "Black"));
-        cars.add(new Car("Ford Mustang", 2021, "Red"));
-        cars.add(new Car("Tesla Model 3", 2022, "White"));
-        cars.add(new Car("Volkswagen Golf", 2018, "Blue"));
-    }
-
-    public List<Car> getCars(int count) {
-        return count < 5 ? cars.subList(0, count) : cars;
-    }
-
-    public List<Car> getAllCars() {
-        return cars;
-    }
+public interface CarService {
+    List<Car> getCars(int count);
+    List<Car> getAllCars();
 }
---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,31 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CarServiceImpl implements CarService {
+
+    private List<Car> cars = new ArrayList<>();
+
+    public CarServiceImpl() {
+        cars.add(new Car("Toyota Camry", 2020, "Silver"));
+        cars.add(new Car("Honda Accord", 2019, "Black"));
+        cars.add(new Car("Ford Mustang", 2021, "Red"));
+        cars.add(new Car("Tesla Model 3", 2022, "White"));
+        cars.add(new Car("Volkswagen Golf", 2018, "Blue"));
+    }
+
+    @Override
+    public List<Car> getCars(int count) {
+        return count < 5 ? cars.subList(0, count) : cars;
+    }
+
+    @Override
+    public List<Car> getAllCars() {
+        return cars;
+    }
+}
